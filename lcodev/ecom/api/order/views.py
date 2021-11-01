bb57e138b2256f064eb9bc5d44aa1b19300c029a
ShahSau <shahriarksaurov@gmail.com>
2021-11-02 01:39:23 +0200

    Order

--- lcodev/ecom/api/order/views.py
+++ lcodev/ecom/api/order/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+
+from api.order.models import Order
+from .serializers import OrderSerializers
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+def validate_user_session(id,token):
+    UserModel = get_user_model()
+
+    try:
+        user= UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please login', 'code':'500'})
+
+    if request.method == 'POST':
+        user_id = id
+        transaction_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['products']
+
+        total_pro = len(products.split(',')[:-1])
+
+        UserModel = get_user_model()
+
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({'error': 'User doesnot exists'})
+        
+        ordr = Order(user=user, product_names = products, total_products=total_pro, transaction_id= transaction_id, total_amount= amount)
+        ordr.save()
+        return JsonResponse({'success': True, 'error': False, 'msg': 'Order placed successfully'})
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    # write the query
+    queryset = Order.objects.all().order_by('id')
+    # what class is called to serialize the data
+    serializer_class = OrderSerializers
--- lcodev/ecom/api/urls.py
+++ lcodev/ecom/api/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', home, name='api.home'),
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
-    path('user/', include('api.user.urls'))
+    path('user/', include('api.user.urls')),
+    path('order/', include('api.order.urls')),
+    # path('payment/', include('api.payment.urls')),
+    #we are not going to use this, we use our own coustom authentication
+    path('api-token-auth/', views.obtain_auth_token, name='api_token_auth')
 ]
